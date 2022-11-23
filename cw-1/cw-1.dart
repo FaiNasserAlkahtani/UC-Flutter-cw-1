23d7dc9ee7cb00611c915b57766537a6cf0778dc
FaiNasserAlkahtani <nsqfai@gmail.com>
2022-11-23 19:57:08 +0300

    ..

---
+++ cw-1/cw-1.dart
@@ -0,0 +1,10 @@
+void main(){
+String FirstName = "Fai";
+String LastName = "Nasser";
+double age = 22.9;
+String hoppy = "painting";
+
+print("First Name : $FirstName");
+print("Last Name : $LastName");
+print("I'm $age, my hoppy is $hoppy");
+}
